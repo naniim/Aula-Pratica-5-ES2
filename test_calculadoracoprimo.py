58e52b8df4115f7b90d992915550181856afe756
naniim <75332203+naniim@users.noreply.github.com>
2023-06-01 13:24:44 -0300

    Update test_calculadoracoprimo.py

--- test_calculadoracoprimo.py
+++ test_calculadoracoprimo.py
@@ -4,12 +4,11 @@ from calculadora_coprimo import CalculadoraCoprimo
 class TestCalculador(unittest.TestCase):
     def setUp(self):
         self.calculadora = CalculadoraCoprimo()
-
     def testMDC(self):
         self.assertEqual(self.calculadora.mdc(12,28),4)
     def testMDCdeZero(self):
         self.assertEqual(self.calculadora.mdc(0,12345),12345)
     def testCoprimosdeUm(self):
-        self.assertEqual|(self.calculadora.QuantosCoprimos(1),0)
+        self.assertEqual(self.calculadora.QuantosCoprimos(1),0)
     def testCoprimos(self):
-        self.assertEqual|(self.calculadora.QuantosCoprimos(10),4)
+        self.assertEqual(self.calculadora.QuantosCoprimos(10),4)
