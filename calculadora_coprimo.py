26f66ebd9215d26445b4fbc408d5e032bd3d6f66
naniim <75332203+naniim@users.noreply.github.com>
2023-06-01 13:25:01 -0300

    Update calculadora_coprimo.py

--- calculadora_coprimo.py
+++ calculadora_coprimo.py
@@ -1,16 +1,18 @@
 class CalculadoraCoprimo:
-    def mdc (x,y):
-        if(x>y):
-            return mdc(y,x)
-        if(x==0):
-            return y;
-        return mdc(x,y%x)
-    
-    def QuantosCoprimos (numero_analisado):
-        quantos = 0;
-        i =1 
-        while(i < numero_analisado):
-            if(mdc(i,numero_analisado)==1):
-                quantos+=1;
-            i+=1
-        return quantos;
+
+  def mdc(self, x, y):
+    if (x > y):
+      return self.mdc(y, x)
+    if (x == 0):
+      return y
+    return self.mdc(x, y % x)
+
+  def QuantosCoprimos(self, numero_analisado):
+    quantos = 0
+    i = 1
+    while (i < numero_analisado):
+      if (self.mdc(i, numero_analisado) == 1):
+        quantos += 1
+      i += 1
+    return quantos
+
